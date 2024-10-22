8ababb180ca77290ec758f93c2139a7f4d4e0d24
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-22 17:54:26 -0400

    Adds test for updating a poster -- TEST PASSES

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -23,4 +23,18 @@ describe "Posters API" do
     expect(created_poster.vintage).to eq(poster_params[:vintage])
     expect(created_poster.img_url).to eq(poster_params[:img_url])
   end
+
+  it "can update an exisiting poster" do
+    id = Poster.create(name: "Shark Bait", description: "Sharks Need To Eat Too!", price: 40.00, year: 2021, vintage: false, img_url: "https://i.ebayimg.com/images/g/oXoAAOSwOAFmqZsG/s-l1600.webp").id
+    old_poster_description = Poster.last.description
+    poster_params = { description: "SCUBA Divers Taste Best!" }
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    patch "/api/v1/posters/#{id}", headers: headers, params: JSON.generate({poster: poster_params})
+    poster = Poster.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(poster.description).to_not eq(old_poster_description)
+    expect(poster.description).to eq("SCUBA Divers Taste Best!")
+  end
 end
