62807a573af15209c208b397c17e61aebfc315d2
Wally Wallace <jrvw001@gmail.com>
2024-10-22 14:41:45 -0600

    Implement fetch all posters

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::PostersController < ApplicationController
 
+  def index
+    render json: Poster.all
+  end
+  
 end
--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -1,4 +1,4 @@
-require rails_helper.rb
+require 'rails_helper'
 
 describe "Posters API" do
   describe "fetch all posters" do
@@ -28,33 +28,34 @@ describe "Posters API" do
       )
     end
 
-    get '/api/v1/posters'
-    expect(response.to be_successful)
-    posters = JSON.parse(response.body, symbolize_names: true)
-    expect(posters.count).to eq(3)
+    it "sends a list of posters" do 
+      get '/api/v1/posters'
+      expect(response).to be_successful
+      posters = JSON.parse(response.body, symbolize_names: true)
+      expect(posters.count).to eq(3)
 
-    posters.each do |poster|
-      expect(poster).to have_key(:id)
-      expect(poster[:id]).to be_an(Integer)
+      posters.each do |poster|
+        expect(poster).to have_key(:id)
+        expect(poster[:id]).to be_an(Integer)
 
-      expect(poster).to have_key(:name)
-      expect(poster[:name]).to be_a(String)
+        expect(poster).to have_key(:name)
+        expect(poster[:name]).to be_a(String)
 
-      expect(poster).to have_key(:description)
-      expect(poster[:description]).to be_a(String)
+        expect(poster).to have_key(:description)
+        expect(poster[:description]).to be_a(String)
 
-      expect(poster).to have_key(:price)
-      expect(poster[:price]).to be_a(Float)
+        expect(poster).to have_key(:price)
+        expect(poster[:price]).to be_a(Float)
 
-      expect(poster).to have_key(:year)
-      expect(poster[:year]).to be_a(Integer)
+        expect(poster).to have_key(:year)
+        expect(poster[:year]).to be_a(Integer)
 
-      expect(poster).to have_key(:vintage)
-      expect(poster[:vintage]).to be_a(Boolean)
+        expect(poster).to have_key(:vintage)
+        expect(poster[:vintage]).to be(true).or be(false)
 
-      expect(poster).to have_key(:img_url)
-      expect(poster[:img_url]).to be_a(String)
+        expect(poster).to have_key(:img_url)
+        expect(poster[:img_url]).to be_a(String)
+      end
     end
-
   end
 end
